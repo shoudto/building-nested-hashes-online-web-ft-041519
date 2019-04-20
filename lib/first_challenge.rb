f3eaac627f7ae696e68ae0fd0d9e4404e67d837c
Oscar Shoudt <shoudto@gmail.com>
2019-04-20 16:26:02 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,35 +1,15 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch => {
-         :name => "Lord Montague",
-         :age => "53"
-       },
-       :matraich => {
-         :name => "Lady Montague",
-         :age => "54"
-       },
-       :hero => {
-         :name => "Romeo",
-         :age => "15",
-         :status => "alive"
-       },
+       :patriarch => {:name => "Lord Montague", :age => "53"},
+       :matraich => {:name => "Lady Montague", :age => "54" },
+       :hero => {:name => "Romeo", :age => "15", :status => "alive"},
        :hero_friends => []
      },
      :capulet => {
-       :patriarch => {
-         :name => "Lord Capulet",
-         :age => "50"
-       },
-       :matraich => {
-         :name => " Lady Capulet",
-         :age => "51"
-       },
-       :heroine => {
-         :name => "Juliet",
-         :age => "15",
-         :status => "alive"
-       }, 
+       :patriarch => {:name => "Lord Capulet", :age => "50"},
+       :matraich => {:name => "Lady Capulet", :age => "51"},
+       :heroine => {:name => "Juliet", :age => "15", :status => "alive"}, 
        :heroine_friends => []
        
      }
