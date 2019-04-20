cc48791d207386eee48494811ab613ff86296fda
Oscar Shoudt <shoudto@gmail.com>
2019-04-20 16:38:54 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,13 +2,13 @@ def third_challenge
   epic_tragedy = {
      :montague => {
        :patriarch => {:name => "Lord Montague", :age => "53"},
-       :matraich => {:name => "Lady Montague", :age => "54" },
+       :matriach => {:name => "Lady Montague", :age => "54" },
        :hero => {:name => "Romeo", :age => "15", :status => "alive"},
        :hero_friends => []
      },
      :capulet => {
        :patriarch => {:name => "Lord Capulet", :age => "50"},
-       :matraich => {:name => "Lady Capulet", :age => "51"},
+       :matriach => {:name => "Lady Capulet", :age => "51"},
        :heroine => {:name => "Juliet", :age => "15", :status => "alive"}, 
        :heroine_friends => []
        
