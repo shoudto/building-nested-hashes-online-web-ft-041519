be34e24985a4a7dab074be6d081f3e751464bedb
Oscar Shoudt <shoudto@gmail.com>
2019-04-20 16:32:25 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {}, 
-      :her_friends => []
+      :hero_friends 
       }, 
     :capulet => {
      :patriarch => {}, 
